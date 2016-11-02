fd967c576551315f1ab51a24a7f69b2127037338
Matt Reed <mreed1272@users.noreply.github.com>
2016-11-02 14:16:39 -0400

    work in progress

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 //const electron = require('electron');
-const remote = electron.remote;
-const dialog = electron.remote.dialog;
-const shell = electron.shell;
-const fs = require('fs');
-const ipcRenderer = electron.ipcRenderer;
+//const remote = electron.remote;
+//const dialog = electron.remote.dialog;
+//const shell = electron.shell;
+//const fs = require('fs');
+//const ipcRenderer = electron.ipcRenderer;
 
 
 
